8dc5e956245e299be1f0598a9d2c8419a676396e
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-08-24 16:15:13 +0800

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,17 @@ export default {
     theme: {
         extend: {
             colors: {
-
+                c_Stone_100: "hsl(30, 54%, 90%)",
+                c_Stone_150: "hsl(30, 18%, 87%)",
+                c_Stone_600: "hsl(30, 10%, 34%)",
+                c_Stone_900: "hsl(24, 5%, 18%)",
+                c_Brown_800: "hsl(14, 45%, 36%)",
+                c_Rose_800: "hsl(332, 51%, 32%)",
+                c_Rose_50: "hsl(330, 100%, 98%)",
             },
             fontFamily: {
-
+                young_serif: ["Young Serif", "serif"],
+                outfit: ["Outfit", "sans-serif"]
             }
         },
     },
